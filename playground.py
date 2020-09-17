10d17f95a9872a9548643cd7cbbd324f1de553cd
Ashley Trinh <ashley@hackbrightacademy.com>
2020-09-17 09:15:46 -0700

    Precompute output if show_output enabled in playground

--- playground.py
+++ playground.py
@@ -16,6 +16,12 @@ from docutils.parsers.rst import Directive, directives
 from docutils.nodes import General, Element, Admonition, literal_block
 from sphinxlectern.handouts.admonitions import BaseAdmonition
 
+import traceback
+import sys
+import html
+from io import StringIO
+from unittest.mock import patch
+
 
 class playground(General, Element):
     pass
@@ -27,18 +33,31 @@ def visit_playground(self, node: playground):
         playground_classes += ["show_output"]
 
     self.body.append(f"<div class=\"{' '.join(playground_classes)}\">")
-    self.body.append('  <div class="buttons">')
-    self.body.append('    <button class="run">')
-    self.body.append('      <i class="fa fa-play"></i>')
-    self.body.append('      <span class="btn-label">Click to run!</span>')
-    self.body.append("    </button>")
-    self.body.append("  </div>")
-    self.body.append('  <div class="editable">')
+    self.body.append('<div class="buttons">')
+    self.body.append('<button class="run">')
+
+    if "show_output" in node:
+        self.body.append('<i class="fa fa-repeat"></i>')
+        self.body.append('<span class="btn-label">Run this again</span>')
+    else:
+        self.body.append('<i class="fa fa-play"></i>')
+        self.body.append('<span class="btn-label">Click to run</span>')
+
+    self.body.append("</button>")
+    self.body.append("</div>")
+    self.body.append('<div class="editable">')
 
 
 def depart_playground(self, node: playground):
-    self.body.append("  </div>")
-    self.body.append('  <pre class="console"></pre>')
+    self.body.append("</div>")
+
+    if "stdout" in node:
+        self.body.append('<pre class="console">')
+        self.body.append(html.escape(node["stdout"], quote=True))
+        self.body.append("</pre>")
+    else:
+        self.body.append('<pre class="console"></pre>')
+
     self.body.append("</div>")
 
 
@@ -48,13 +67,27 @@ class Playground(Directive):
     has_content = True
     option_spec = {"show_output": directives.flag, "editable": directives.flag}
 
+    def exec_code(self, code_str):
+        try:
+            namespace = {"__name__": "__main__"}
+            exec(code_str, namespace)
+        except Exception:
+            traceback.print_exc(file=sys.stdout)
+
     def run(self) -> List[playground]:
         code = "\n".join(self.content)
         node = playground(code, **self.options)
         self.add_name(node)
+
+        if "show_output" in self.options:
+            with patch("sys.stdout", new_callable=StringIO) as string_stdout:
+                self.exec_code(code)
+                node["stdout"] = string_stdout.getvalue()
+
         lit_node = literal_block(code, code)
         lit_node["language"] = "python"
         node += lit_node
+
         return [node]
 
 
